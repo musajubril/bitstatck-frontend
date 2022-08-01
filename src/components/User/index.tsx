c7ef4360fd3fbbd9d1b50a10d8eeb2063caf6015
Jubril Musa <41811898+Muhsar@users.noreply.github.com>
2022-08-01 04:21:42 +0100

    added dashboard ui

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -4,6 +4,7 @@ import { queryKeys } from 'api/queryKey'
 import React from 'react'
 import moment from 'moment'
 import { useQuery } from 'react-query'
+import Layout from 'components/Layout2'
 
 export default function User() {
     const {data} = useQuery(
@@ -16,9 +17,11 @@ export default function User() {
     const [user, setUser] = React.useState<any>(data?.data)
     React.useEffect(()=> {
         setUser(data?.data)
+        // localStorage.setItem("user", JSON.stringify(data?.data))
     },[data?.data])
     console.log(user)
   return (
+    <Layout>
     <div className="h-screen w-full flex flex-col items-center justify-center">
         <div className="">
             <img src={require('../../images/Welcome.gif')} alt="" />
@@ -39,5 +42,6 @@ export default function User() {
             }
         </div>
     </div>
+    </Layout>
   )
 }
